a06637f1e10ba02085718ee1c16f848e3a5c24e8
Muhammad Amin Hidayat <tumbaldaya@gmail.com>
2025-12-31 20:18:03 +0800

    stag: prisma orm

--- novii-bus/prisma/seed.ts
+++ novii-bus/prisma/seed.ts
@@ -1,6 +1,18 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 
-const prisma = new PrismaClient()
+const { Pool } = pg
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+
+const prisma = new PrismaClient({
+  adapter,
+})
 
 async function main() {
   console.log('🌱 Seeding database...')
